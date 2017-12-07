1368dab47c18b8cbe0c9974e4613f90bb3093d14
Carlos Arcenas <carlosarcenas@yahoo.com>
2017-12-07 08:26:04 +0800

    Add visualization barebones

---
+++ visualization/deckgl-overlay.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import DeckGL, {PolygonLayer, GeoJsonLayer} from 'deck.gl';
+// import TripsLayer from './trips-layer';
+
+const LIGHT_SETTINGS = {
+  lightsPosition: [-74.05, 40.7, 8000, -73.5, 41, 5000],
+  ambientRatio: 0.05,
+  diffuseRatio: 0.6,
+  specularRatio: 0.8,
+  lightsStrength: [2.0, 0.0, 0.0, 0.0],
+  numberOfLights: 2
+};
+
+export default class DeckGLOverlay extends Component {
+
+  static get defaultViewport() {
+    return {
+      longitude: -74,
+      latitude: 40.72,
+      zoom: 13,
+      maxZoom: 16,
+      pitch: 45,
+      bearing: 0
+    };
+  }
+
+  render() {
+    const {viewport, buildings, boroughs, trips, trailLength, time} = this.props;
+
+    if (!buildings) {
+      return null;
+    }
+
+    const layers = [
+      new PolygonLayer({
+        id: 'buildings',
+        data: buildings,
+        extruded: true,
+        wireframe: false,
+        fp64: true,
+        opacity: 0.5,
+        getPolygon: f => f.polygon,
+        getElevation: f => f.height,
+        getFillColor: f => [74, 80, 87],
+        lightSettings: LIGHT_SETTINGS
+      }),
+      new GeoJsonLayer({
+        id: 'boroughs',
+        data: boroughs,
+        filled: true,
+        stroked: false,
+        extruded: true
+      })
+    ];
+
+    return (
+      <DeckGL {...viewport} layers={layers} initWebGLParameters />
+    );
+  }
+}
