fc6a673409140802faff43f54996dd231e098e67
Carlos Arcenas <carlosarcenas@yahoo.com>
2017-12-07 14:14:29 +0800

    Finalize display of boroughs and trip pickup points

--- visualization/deckgl-overlay.js
+++ visualization/deckgl-overlay.js
@@ -1,6 +1,9 @@
+// ADAPTED FROM DECK.GL "TRIPLAYERS EXAMPLE"
+// https://github.com/uber/deck.gl/tree/master/examples/trips
+// Uber Technologies Inc., 2017
+
 import React, {Component} from 'react';
 import DeckGL, {PolygonLayer, GeoJsonLayer} from 'deck.gl';
-// import TripsLayer from './trips-layer';
 
 const LIGHT_SETTINGS = {
   lightsPosition: [-74.05, 40.7, 8000, -73.5, 41, 5000],
@@ -25,7 +28,11 @@ export default class DeckGLOverlay extends Component {
   }
 
   render() {
-    const {viewport, boroughs, trips, trailLength, time} = this.props;
+    const {viewport, boroughs, trips} = this.props;
+
+    if (!trips || !boroughs) {
+      return null;
+    }
 
     const layers = [
       new GeoJsonLayer({
@@ -37,6 +44,22 @@ export default class DeckGLOverlay extends Component {
         getFillColor: f => f.properties.color
       })
     ];
+
+    console.log("Received trips:");
+    console.log(trips);
+    for (const boroughIndex in trips) {
+      layers.push(new GeoJsonLayer({
+        id: 'borough' + boroughIndex,
+        data: trips[boroughIndex],
+        filled: true,
+        stroked: true,
+        extruded: false,
+        getFillColor: f => f.properties.color,
+        getRadius: f => 25
+      }));
+    }
+
+    console.log(layers);
 
     return (
       <DeckGL {...viewport} layers={layers} initWebGLParameters />
